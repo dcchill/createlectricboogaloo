e8061c97c1cc905750b88001ea5126e3fa60e740
daria <daria@DariansPC>
2023-06-15 07:53:04 -0400

    MCreator Merge commit 'fcbd7e39f8989494378e66fa02b782f1bb1073b4' into 5e7abfd036a002261e87fbbe5f7dc9e40adde04d

---
+++ src/main/java/net/createelectricboogaloo/init/CreateelectricboogalooModItems.java
@@ -0,0 +1,27 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.createelectricboogaloo.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.BlockItem;
+
+import net.createelectricboogaloo.CreateelectricboogalooMod;
+
+public class CreateelectricboogalooModItems {
+	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, CreateelectricboogalooMod.MODID);
+	public static final RegistryObject<Item> COMBUSSIN_ENGINE_PART = block(CreateelectricboogalooModBlocks.COMBUSSIN_ENGINE_PART, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> COMBUSSIN_ENGINE = REGISTRY.register(CreateelectricboogalooModBlocks.COMBUSSIN_ENGINE.getId().getPath(),
+			() -> new CombussinEngineDisplayItem(CreateelectricboogalooModBlocks.COMBUSSIN_ENGINE.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+
+	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
+		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+	}
+}
